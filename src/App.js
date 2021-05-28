7d7f1585943af362c44b9cf3a9a1c83f2c17fcdd
vanesa <vanesa@vanesas-MacBook-Pro.local>
2021-05-28 19:36:49 -0400

    redeploy

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+ 
 import './App.css';
 import Employee from "./pages/employee"
 import Wrapper from "./components/Wrapper";
@@ -12,13 +12,13 @@ import Wrapper from "./components/Wrapper";
 //add the components with in this class app 
 function App() {
   return (
-      <Router>
+      
         <div> 
           <Wrapper>
-             <Route exact path="/" component={Employee}  />
+             <Employee   />
           </Wrapper>
         </div>
-      </Router>
+    
   );
 }
 
